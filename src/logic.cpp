e8502dd4364d9de5188c2e41d1ac23f9ab5df6e5
Jamie Bray <jamie@jamiebray.xyz>
2021-09-29 00:03:09 +0800

    Fix random board generator

--- src/logic.cpp
+++ src/logic.cpp
@@ -10,8 +10,8 @@ using NeighbourPositions = std::array<Position, 8>;
 
 Board randomBoard(int width, int height) {
   Board board(height, std::vector<bool>(width));
-  for (int y = 0; y < 600; ++y) {
-    for (int x = 0; x < 600; ++x) {
+  for (int y = 0; y < height; ++y) {
+    for (int x = 0; x < width; ++x) {
       board[y][x] = rand() % 2;
     }
   }
