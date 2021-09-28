65ab5bf9108fe40d657fdee9ffe2dc16c1739030
Jamie Bray <jamie@jamiebray.xyz>
2021-09-29 00:14:49 +0800

    Clean up graphics fps metrics

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -4,6 +4,17 @@
 #include <chrono>
 #include <iostream>
 
+using milliseconds = std::chrono::milliseconds;
+
+void tilNow(std::string label,
+            std::chrono::time_point<std::chrono::steady_clock> start) {
+  std::cout << label << " time: "
+            << std::chrono::duration_cast<milliseconds>(
+                   std::chrono::steady_clock::now() - start)
+                   .count()
+            << " ms" << std::endl;
+}
+
 int main() {
   const int width = 600;
   const int height = 600;
@@ -14,13 +25,12 @@ int main() {
   SDL_Init(SDL_INIT_VIDEO);
   SDL_CreateWindowAndRenderer(width, height, 0, &window, &renderer);
 
-  auto begin = std::chrono::steady_clock::now();
-  int frames = 0;
-  int exit = 0;
-
   Board board = randomBoard(width, height);
 
+  bool exit = false;
   while (!exit) {
+    auto startSdl = std::chrono::steady_clock::now();
+
     for (int y = 0; y < 600; ++y)
       for (int x = 0; x < 600; ++x) {
         if (board[y][x])
@@ -31,29 +41,20 @@ int main() {
       }
 
     SDL_RenderPresent(renderer);
-
-    board = nextBoard(board);
-
     while (SDL_PollEvent(&event))
       if (event.key.keysym.sym == SDLK_ESCAPE)
-        exit = 1;
+        exit = true;
 
-    frames++;
-    if (frames > 60) {
-      auto end = std::chrono::steady_clock::now();
-      std::cout << "Microseconds per 60 frames: "
-                << std::chrono::duration_cast<std::chrono::microseconds>(end -
-                                                                         begin)
-                       .count()
-                << std::endl;
-      begin = std::chrono::steady_clock::now();
-      frames = 0;
-    }
+    tilNow("SDL", startSdl);
+
+    auto startBoard = std::chrono::steady_clock::now();
+    board = nextBoard(board);
+    tilNow("Board computation", startBoard);
   }
+
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
-  return EXIT_SUCCESS;
 
-  return 0;
+  return EXIT_SUCCESS;
 }
