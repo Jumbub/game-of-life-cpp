f64f4adbf7c6f956d2edcff397483de44ccaabec
Jamie Bray <jamie@jamiebray.xyz>
2021-10-01 01:42:45 +0800

    40ms -> 23ms, by inlining the neighbour calculation, also fixed the test suite

--- src/board/next.cpp
+++ src/board/next.cpp
@@ -6,28 +6,6 @@
 #include <tuple>
 #include <vector>
 
-const NeighbourPositions getNeighbourPositions(const int px, const int py,
-                                               const int width,
-                                               const int height) {
-  NeighbourPositions positions = {};
-  int i = 0;
-  for (int y = -1; y <= 1; y++)
-    for (int x = -1; x <= 1; x++) {
-      if (y == 0 && x == 0)
-        continue;
-
-      auto xx = (px + x) % width;
-      if (xx < 0)
-        xx = xx + width;
-      auto yy = (py + y) % height;
-      if (yy < 0)
-        yy = yy + height;
-
-      positions[i++] = yy * width + xx;
-    }
-  return positions;
-}
-
 Board nextBoard(Board board) {
   const auto &[input, width, height] = board;
   if (height == 0)
@@ -43,17 +21,27 @@ Board nextBoard(Board board) {
 
       auto alive = input[i];
 
-      auto neighboursPositions = getNeighbourPositions(x, y, width, height);
       int neighboursCount = 0;
-      for (int n = 0; n < 8; n++) {
-        if (input[neighboursPositions[n]])
-          neighboursCount++;
+      for (int y2 = -1; y2 <= 1; y2++)
+        for (int x2 = -1; x2 <= 1; x2++) {
+          if (y2 == 0 && x2 == 0)
+            continue;
 
-        // Optimisation (exit early if we know it's a dead cell)
-        if (neighboursCount > 3) {
-          break;
+          auto xx = (x2 + x) % width;
+          if (xx < 0)
+            xx = xx + width;
+          auto yy = (y2 + y) % height;
+          if (yy < 0)
+            yy = yy + height;
+
+          if (input[yy * width + xx])
+            neighboursCount++;
+
+          // Optimisation (exit early if we know it's a dead cell)
+          if (neighboursCount > 3) {
+            break;
+          }
         }
-      }
 
       if (alive && (neighboursCount < 2 || neighboursCount > 3))
         output[i] = false;
