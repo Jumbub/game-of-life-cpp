d1a9537045bf04e380814381649ea83faa789439
Jamie Bray <jamie@jamiebray.xyz>
2021-10-01 17:02:19 +0800

    Improve profiler

--- src/util/profile.cpp
+++ src/util/profile.cpp
@@ -1,15 +1,26 @@
 #include "profile.h"
 #include <chrono>
 #include <iostream>
-
-using milliseconds = std::chrono::milliseconds;
+#include <iomanip>
 
 using ProfileData = std::chrono::time_point<std::chrono::steady_clock>;
 ProfileData startProfiling() { return std::chrono::steady_clock::now(); }
 void stopProfiling(ProfileData startTime, std::string label) {
-  std::cout << label << " time: "
-            << std::chrono::duration_cast<milliseconds>(
-                   std::chrono::steady_clock::now() - startTime)
-                   .count()
-            << " ms" << std::endl;
+  const auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+                      std::chrono::steady_clock::now() - startTime)
+                      .count();
+  const auto mcs = std::chrono::duration_cast<std::chrono::microseconds>(
+                      std::chrono::steady_clock::now() - startTime)
+                      .count();
+  const auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(
+                      std::chrono::steady_clock::now() - startTime)
+                      .count();
+
+  if (mcs < 5) {
+    std::cout << std::setw (8) << ns << " nanos  | " << label << std::endl;
+  } else if (ms < 5) {
+    std::cout << std::setw (8) << mcs << " micros | " << label << std::endl;
+  } else {
+    std::cout << std::setw (8) << ms << " millis | " << label << std::endl;
+  }
 }
