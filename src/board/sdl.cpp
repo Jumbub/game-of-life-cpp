9e949115fd6289cda0a43c9310527973856fb5cc
Jamie Bray <jamie@jamiebray.xyz>
2021-10-02 16:37:29 +0800

    Saving about 100 microseconds replacing vector push_back with array manipulation

--- src/board/sdl.cpp
+++ src/board/sdl.cpp
@@ -11,17 +11,19 @@ Board boardForSdlWindow(SDL_Window *window) {
 void renderBoardSdl(Board board, SDL_Renderer *renderer) {
   const auto &[input, width, height] = board;
 
-  std::vector<SDL_Point> points;
-  for (int y = 0; y < height; ++y)
-    for (int x = 0; x < width; ++x)
-      if (input[y * width + x])
-        points.push_back({x, y});
+  auto points = std::unique_ptr<SDL_Point[]>(new SDL_Point[width * height]);
+  int count = 0;
+  for (int i = 0; i < width * height; i++)
+    if (input[i]) {
+      points[count] = {i % width, i / width};
+      count++;
+    }
 
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   SDL_RenderClear(renderer);
 
   SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-  SDL_RenderDrawPoints(renderer, &points[0], points.size());
+  SDL_RenderDrawPoints(renderer, &points[0], count);
 
   SDL_RenderPresent(renderer);
 }
