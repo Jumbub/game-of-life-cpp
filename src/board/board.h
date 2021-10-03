1f15ef4009b0c71234e9220a1d4e9c57922fd5e1
Jamie Bray <jamie@jamiebray.xyz>
2021-10-03 13:16:14 +0800

    Remove SDL dependency from board types

--- src/board/board.h
+++ src/board/board.h
@@ -4,10 +4,9 @@
 #include <tuple>
 #include <array>
 #include <memory>
-#include <SDL2/SDL.h>
 
-using Cell = Uint32;
-const Cell ALIVE = SDL_MAX_UINT32;
+using Cell = unsigned int; // Should match SDL::Uint32, otherwise the rendering will not work as intended
+const Cell ALIVE = UINT32_MAX; // Should match SDL::SDL_UINT32_MAX, "
 const Cell DEAD = 0;
 
 using Board = std::tuple<Cell*, int, int>;
--- src/board/sdl.cpp
+++ src/board/sdl.cpp
@@ -12,6 +12,7 @@ Board boardForSdlWindow(SDL_Window *window) {
 void renderBoardSdl(Board board, SDL_Renderer *renderer, SDL_Texture *texture) {
   const auto &[input, width, height] = board;
 
+  static_assert(sizeof(input[0]) == sizeof(Uint32));
   SDL_UpdateTexture(texture, NULL, &input[0], width * sizeof(Uint32));
 
   SDL_RenderCopy(renderer, texture, nullptr, nullptr);
