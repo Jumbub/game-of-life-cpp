cfd8c82936465c517d28f68b94d43abae289dc7d
Jamie Bray <jamie@jamiebray.xyz>
2021-10-03 17:13:50 +0800

    Minimise lines of code

--- src/board/next.cpp
+++ src/board/next.cpp
@@ -22,7 +22,6 @@ Board nextBoard(const Board &board) {
 
   for (int i = 0; i < width * height; i++) {
     const int x = i % width;
-    auto currentState = input[i];
     auto currentStateBool = input[i] == ALIVE ? 1 : 0;
 
     // Slide neighbours
@@ -30,7 +29,6 @@ Board nextBoard(const Board &board) {
       // Clear neighbour columns
       neighbours[0] = -1;
       neighbours[1] = -1;
-      neighbours[2] = -1;
 
       // Compute new Y levels
       const int y = i / width;
@@ -38,13 +36,9 @@ Board nextBoard(const Board &board) {
       yBelowBase = ((y - 1 + height) % height) * width;
       yAboveBase = ((y + 1) % height) * width;
     } else {
-      // Remove self from right neighbour column
-      neighbours[2] -= currentStateBool;
-
-      // Shift neighbour columns
       neighbours[0] = neighbours[1];
-      neighbours[1] = neighbours[2];
-      neighbours[2] = -1;
+      // Remove own value
+      neighbours[1] = neighbours[2] - currentStateBool;
     }
 
     // Compute neighbours
@@ -59,22 +53,19 @@ Board nextBoard(const Board &board) {
       neighbours[1] = (input[yBelowBase + x] ? 1 : 0) +
                       (input[yAboveBase + x] ? 1 : 0);
     }
-    if (neighbours[2] == -1) {
-      const auto nextX = (x + 1) % width;
-
-      neighbours[2] = (input[yBelowBase + nextX] ? 1 : 0) +
-                      (input[yBase + nextX] ? 1 : 0) +
-                      (input[yAboveBase + nextX] ? 1 : 0);
-    }
+    const auto nextX = (x + 1) % width;
+    neighbours[2] = (input[yBelowBase + nextX] ? 1 : 0) +
+                    (input[yBase + nextX] ? 1 : 0) +
+                    (input[yAboveBase + nextX] ? 1 : 0);
 
     // Compute new cell state
     const int totalNeighbours = neighbours[0] + neighbours[1] + neighbours[2];
-    if (currentState && (totalNeighbours < 2 || totalNeighbours > 3))
+    if (currentStateBool && (totalNeighbours < 2 || totalNeighbours > 3))
       output[i] = DEAD;
-    else if (!currentState && totalNeighbours == 3)
+    else if (!currentStateBool && totalNeighbours == 3)
       output[i] = ALIVE;
     else
-      output[i] = currentState;
+      output[i] = input[i];
 
     // Add self to neighbours count
     neighbours[1] += currentStateBool;
