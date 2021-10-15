66d6de141f4c17346eb97a495e0d6d34874b7950
Jamie Bray <jamie@jamiebray.xyz>
2021-10-16 03:07:14 +0800

    499bps -> 599bps. Skipping ahead by reading uint8 cells as groups in uint64s

--- src/board/next.h
+++ src/board/next.h
@@ -11,25 +11,29 @@
 #include "padding.h"
 #include "threads.h"
 
+const Cell EMPTY = 16;
+
+uint64_t val64(Cell* ns) {
+  return *reinterpret_cast<uint64_t*>(ns);
+}
+
 void nextBoardSection(
     const uint startY,
     const uint endY,
     const uint width,
-    const Cell* input,
+    Cell* input,
     Cell* output) {
   Cell neighbours[3] = {0, 0, 0};
   uint nextYBase = 0;
   uint middleYBase = 0;
   uint lastYBase = 0;
 
-  const Cell* neighboursBelow = nullptr;
-  const Cell* neighboursMiddle = nullptr;
-  const Cell* neighboursAbove = nullptr;
+  Cell* neighboursBelow = nullptr;
+  Cell* neighboursMiddle = nullptr;
+  Cell* neighboursAbove = nullptr;
 
   const auto realWidth = width + 2;
   for (uint y = startY + 1; y < endY + 1; y++) {
-    const auto currentStateBool = input[y * realWidth + 1];
-
     // Compute new Y levels
     lastYBase = ((y - 1) * realWidth);
     middleYBase = y * realWidth;
@@ -39,21 +43,43 @@ void nextBoardSection(
     neighboursMiddle = &input[middleYBase];
     neighboursAbove = &input[nextYBase];
 
-    // Left neighbours
-    neighbours[0] =
-        neighboursBelow[0] + neighboursMiddle[0] + neighboursAbove[0];
-
-    // Middle neighbours
-    neighbours[1] = neighboursBelow[1] + neighboursAbove[1] + currentStateBool;
+    neighbours[0] = EMPTY;
+    neighbours[1] = EMPTY;
 
     for (uint x = 1; x < width + 1; x++) {
       const auto i = y * realWidth + x;
+
+      // Skip if we see sequential 0 neighbour counts
+      constexpr uint MAXIMUM_SKIP = 6;
+      if (x < (width+2-MAXIMUM_SKIP) && val64(&neighboursAbove[x-1]) + val64(&neighboursMiddle[x-1])+ val64(&neighboursBelow[x-1]) == 0) {
+        neighbours[0] = EMPTY;
+        neighbours[1] = EMPTY;
+        neighbours[2] = EMPTY;
+        for (uint ii = 0; ii < MAXIMUM_SKIP; ii++)
+          output[i + ii] = DEAD;
+        x+=MAXIMUM_SKIP-1;
+        continue;
+      }
+
       const auto currentStateBool = input[i];
 
       if (x > 1) {
         // Shift neighbour counts down
         neighbours[0] = neighbours[1];
         neighbours[1] = neighbours[2];
+      }
+
+      // Left neighbours
+      if (neighbours[0] == EMPTY) {
+        const auto prevX = x - 1;
+        neighbours[0] = neighboursBelow[prevX] + neighboursMiddle[prevX] +
+                        neighboursAbove[prevX];
+      }
+
+      // Middle neighbours
+      if (neighbours[1] == EMPTY) {
+        neighbours[1] =
+            neighboursBelow[x] + neighboursAbove[x] + currentStateBool;
       }
 
       const auto nextX = x + 1;
@@ -95,10 +121,9 @@ void nextBoard(const BoardMeta& board) {
     if (t == totalThreads - 1)
       endY += threadLinesRemaining;
 
-    threads.push_back(
-        std::thread([startY, endY, width, input, output]() {
-          nextBoardSection(startY, endY, width, input, output);
-        }));
+    threads.push_back(std::thread([startY, endY, width, input, output]() {
+      nextBoardSection(startY, endY, width, input, output);
+    }));
   }
 
   for (auto& thread : threads) {
--- src/entrypoints/test.cpp
+++ src/entrypoints/test.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN
 
-#include <catch2/catch.hpp>
 #include <array>
+#include <catch2/catch.hpp>
 #include "../board/next.h"
 #include "../board/padding.h"
 
@@ -12,9 +12,9 @@ void generate(BoardMeta& board, BoardVector vector, bool a = true) {
   const auto width = (unsigned int)vector[0].size();
 
   board.resize(width, height);
-  for (unsigned int y = 1; y < height+1; ++y)
-    for (unsigned int x = 1; x < width+1; ++x)
-      board.input[y*(width+2) + x] = vector[y-1][x-1] ? ALIVE : DEAD;
+  for (unsigned int y = 1; y < height + 1; ++y)
+    for (unsigned int x = 1; x < width + 1; ++x)
+      board.input[y * (width + 2) + x] = vector[y - 1][x - 1] ? ALIVE : DEAD;
 
   padding(board.input, width, height);
 
@@ -28,16 +28,16 @@ BoardVector ungenerate(BoardMeta& board) {
   const auto& height = board.height;
 
   std::vector<std::vector<bool>> vector(height, std::vector<bool>(width));
-  for (unsigned int y = 1; y < height+1; ++y)
-    for (unsigned int x = 1; x < width+1; ++x)
-      vector[y-1][x-1] = output[y * (width+2) + x] == ALIVE ? 1 : 0;
+  for (unsigned int y = 1; y < height + 1; ++y)
+    for (unsigned int x = 1; x < width + 1; ++x)
+      vector[y - 1][x - 1] = output[y * (width + 2) + x] == ALIVE ? 1 : 0;
 
   return vector;
 }
 
 void compare(BoardVector a, BoardVector b) {
-  auto boardA = BoardMeta(1,1);
-  auto boardB = BoardMeta(1,1);
+  auto boardA = BoardMeta(1, 1);
+  auto boardB = BoardMeta(1, 1);
   generate(boardA, a, true);
   generate(boardB, b, false);
   nextBoard(boardA);
@@ -294,7 +294,9 @@ TEST_CASE("glider 4", "[nextBoard]") {
        {0, 0, 0, 0, 0}});
 }
 
-TEST_CASE("gliders wide horizontal wrap collision [long board]", "[nextBoard]") {
+TEST_CASE(
+    "gliders wide horizontal wrap collision [long board]",
+    "[nextBoard]") {
   compare(
       {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
        {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
@@ -346,37 +348,56 @@ TEST_CASE("glider tall vertical wrap collision [tall board]", "[nextBoard]") {
        {0, 0, 1, 1, 0}});
 }
 
+TEST_CASE("glider 1 large", "[nextBoard]") {
+  compare(
+      {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+
+      {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}});
+}
+
 TEST_CASE("2x2", "[padding]") {
-  std::vector<Cell>input{
-    0, 0, 0, 0,
-    0, 1, 0, 0,
-    0, 0, 1, 0,
-    0, 0, 0, 0,
+  std::vector<Cell> input{
+      0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
   };
-  std::vector<Cell>expected{
-    1, 0, 1, 0,
-    0, 1, 0, 1,
-    1, 0, 1, 0,
-    0, 1, 0, 1,
+  std::vector<Cell> expected{
+      1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1,
   };
   padding(&input[0], 2, 2);
   REQUIRE(input == expected);
 }
 
 TEST_CASE("4x3", "[padding]") {
-  std::vector<Cell>input{
-    0, 0, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 0,
-    0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0,
+  std::vector<Cell> input{
+      0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   };
-  std::vector<Cell>expected{
-    0, 0, 0, 0, 0, 0,
-    1, 1, 1, 1, 1, 1,
-    0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0,
-    1, 1, 1, 1, 1, 1,
+  std::vector<Cell> expected{
+      0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1,
   };
   padding(&input[0], 4, 3);
   REQUIRE(input == expected);
