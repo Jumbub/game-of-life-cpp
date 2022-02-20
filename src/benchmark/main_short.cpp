82d0c6097e230036b8a0427270afaf1490e7b2f2
Jamie Bray <jamie@jamiebray.xyz>
2022-02-21 00:57:16 +0800

    Update the optimal thread count to again match the best results from the bench_all test

--- src/benchmark/main_short.cpp
+++ src/benchmark/main_short.cpp
@@ -7,7 +7,7 @@
 static void BM_DifferentThreadCounts(benchmark::State& state) {
   Loop loop(2560, 1440, "Game of Speed [Benchmark]", false);
   for (auto _ : state) {
-    loop.run(2000, state.range(0), 30);
+    loop.run(2000, state.range(0), 1000000 / 30);
   }
 }
 
--- src/logic/threads.h
+++ src/logic/threads.h
@@ -3,4 +3,4 @@
 #include <thread>
 
 // The number of threads that was optimal in the benchmarks
-const uint PROBABLY_OPTIMAL_THREAD_COUNT = std::max(std::thread::hardware_concurrency() * 3 - 1, (unsigned int)1);
+const uint PROBABLY_OPTIMAL_THREAD_COUNT = std::max(std::thread::hardware_concurrency(), (unsigned int)1);
