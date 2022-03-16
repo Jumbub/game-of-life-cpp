35ae98778621cb901e345a95c41efefe51b6dd37
Jamie Bray <jamie@jamiebray.xyz>
2022-03-16 23:41:04 +0800

    Use setters

--- src/logic/padding.cpp
+++ src/logic/padding.cpp
@@ -34,21 +34,21 @@ void assignJobs(Board& board) {
   const auto height = board.paddedHeight;
 
   for (uint i = 1; i <= width - 1; i++) {
-    cells.set(i + width * innerHeight, cells.test(i + width * innerHeight) & cells.test(i));
+    cells.set(i + width * innerHeight, cells.test(i + width * innerHeight) && cells.test(i));
   }
   for (uint i = width * (height - 1) + 1; i <= width * height - 2; i++) {
-    cells.set(i - width * innerHeight, cells.test(i - width * innerHeight) & cells.test(i));
+    cells.set(i - width * innerHeight, cells.test(i - width * innerHeight) && cells.test(i));
   }
   for (uint i = width; i <= width * (height - 1); i += width) {
-    cells.set(i + innerWidth, cells.test(i + innerWidth) & cells.test(i));
+    cells.set(i + innerWidth, cells.test(i + innerWidth) && cells.test(i));
   }
   for (uint i = (width * 2) - 1; i <= width * (height - 1) - 1; i += width) {
-    cells.set(i - innerWidth, cells.test(i - innerWidth) & cells.test(i));
+    cells.set(i - innerWidth, cells.test(i - innerWidth) && cells.test(i));
   }
-  cells.set(0, cells.test(0) & cells.test(width * (height - 1) - 2));
-  cells.set(width - 1, cells.test(width - 1) & cells.test(width * (height - 2) + 1));
-  cells.set(width * (height - 1), cells.test(width * (height - 1)) & cells.test(width * 2 - 2));
-  cells.set(width * height - 1, cells.test(width * height - 1) & cells.test(width + 1));
+  cells.set(0, cells.test(0) && cells.test(width * (height - 1) - 2));
+  cells.set(width - 1, cells.test(width - 1) && cells.test(width * (height - 2) + 1));
+  cells.set(width * (height - 1), cells.test(width * (height - 1)) && cells.test(width * 2 - 2));
+  cells.set(width * height - 1, cells.test(width * height - 1) && cells.test(width + 1));
 }
 
 void assignBoardPadding(Board& board) {
