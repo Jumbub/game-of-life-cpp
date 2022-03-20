34755bb66095523d7d0f33b2224ae577f5d8c73e
Jamie Bray <jamie@jamiebray.xyz>
2022-03-20 22:01:39 +0800

    Revert back to simple black and white for clarity

--- src/graphics/renderBoard.cpp
+++ src/graphics/renderBoard.cpp
@@ -7,13 +7,13 @@
 #include "../logic/board.h"
 #include "renderBoard.h"
 
-constexpr uint32_t COLOR_DEAD = 0x10dd0000;
-constexpr uint32_t COLOR_ALIVE = 0xffffffff - COLOR_DEAD;
+constexpr uint64_t BLACK = 0xff000000;
+constexpr uint64_t WHITE_TRANSPARENT = 0x00ffffff;
 
 void renderBoard(Board& board, sf::RenderWindow& window, sf::Sprite& sprite, sf::Texture& texture, sf::Uint32* pixels) {
   const uint limitI = board.rawWidth * board.rawHeight - board.rawWidth;
   for (uint i = board.rawWidth; i < limitI; i++) {
-    pixels[i] = COLOR_DEAD + COLOR_ALIVE * board.output[i];
+    pixels[i] = BLACK + WHITE_TRANSPARENT * board.output[i];
   }
 
   texture.update(reinterpret_cast<sf::Uint8*>(pixels));
