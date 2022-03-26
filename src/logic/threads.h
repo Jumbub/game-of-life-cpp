e40990f63d2c3e706c4fd6d128e8aab979195a5d
Jamie Bray <jamie@jamiebray.xyz>
2022-03-26 23:38:53 +0800

    Another

--- src/logic/threads.h
+++ src/logic/threads.h
@@ -4,4 +4,5 @@
 
 // The number of threads that was optimal in the benchmarks
 const uint PROBABLY_OPTIMAL_THREAD_COUNT = std::max(std::thread::hardware_concurrency(), (unsigned int)1);
+// TODO: change this to some multiple of thread count
 const uint PROBABLY_OPTIMAL_JOB_COUNT = 16;
