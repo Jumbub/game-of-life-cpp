f57954a3f48fadb06837ca4e7b6340bd60a49525
Jamie Bray <jamie@jamiebray.xyz>
2022-07-01 17:52:35 +0800

    Probably optimal job count

--- src/logic/threads.h
+++ src/logic/threads.h
@@ -4,4 +4,4 @@
 
 // The number of threads that was optimal in the benchmarks
 const uint PROBABLY_OPTIMAL_THREAD_COUNT = std::max(std::thread::hardware_concurrency(), (unsigned int)1);
-const uint PROBABLY_OPTIMAL_JOB_COUNT = 16;
+const uint PROBABLY_OPTIMAL_JOB_COUNT = 4*PROBABLY_OPTIMAL_THREAD_COUNT;
